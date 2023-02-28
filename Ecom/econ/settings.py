840773dfba594087b7207a0a780b7aa57e448127
James Alhassan <jamesalhassan052@gmail.com>
2023-02-28 18:12:29 +0000

    Add dynamic product names and prices

--- Ecom/econ/settings.py
+++ Ecom/econ/settings.py
@@ -122,5 +122,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+import os
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+MEDIA_ROOT = os.path.join(BASE_DIR / "static/images")
